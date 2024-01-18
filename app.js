0f5c45f51a29902cd0dae35458f302f4ec2cc83f
Oscar <ozkin1vs@hotmail.com>
2024-01-18 11:51:59 -0500

    aprendineod sobre bucles js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 // este son las variables
 let secretNumber = 6;
-let numUser = prompt("Me indicas un numero  entre 1 y 10 porfavor");
-console.log(numUser);
-//este codigo realiza la confirmacion
-if(numUser==secretNumber){
-    alert(`numero correcto es ${secretNumber}`);
-} else{
-    if(numUser>secretNumber){
-        alert('el numero es menor');
-    }else{
-        alert('el numero es mayor');
+let numUser=0;
+while (numUser!=secretNumber) {
+    numUser = prompt("Me indicas un numero  entre 1 y 10 porfavor");
+    console.log(numUser);
+    //este codigo realiza la confirmacion
+    if (numUser == secretNumber) {
+        alert(`numero correcto es ${secretNumber}`);
+    } else {
+        if (numUser > secretNumber) {
+            alert('el numero es menor');
+        } else {
+            alert('el numero es mayor');
+        }
+        alert('No acertaste');
     }
-    alert('No acertaste');
 }
